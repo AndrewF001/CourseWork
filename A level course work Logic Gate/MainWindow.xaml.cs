a91e03a7b0763974448256dfe98cd47dcf0e3c00
AndrewF001 <42306036+AndrewF001@users.noreply.github.com>
2020-03-19 09:25:01 +0000

    Finished V2
    Updated write up

--- A level course work Logic Gate/MainWindow.xaml.cs
+++ A level course work Logic Gate/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media; 
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
